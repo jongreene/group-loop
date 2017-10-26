02759e3bb67b0939e4f07ff7220cee5dad4e6f57
Jon Greene <jongreene@ku.edu>
2017-10-26 18:55:36 -0500

    Added main activities

---
+++ app/src/main/java/ferocioushammerheads/grouploop/GroupItems.java
@@ -0,0 +1,13 @@
+package ferocioushammerheads.grouploop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_items);
+    }
+}

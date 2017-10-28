79400f637dba421d02af1c641f257090436bc7ae
Jon Greene <jongreene@ku.edu>
2017-10-28 18:55:16 -0500

    Setup shared sessions across activities.

--- app/src/main/java/ferocioushammerheads/grouploop/MainActivity.java
+++ app/src/main/java/ferocioushammerheads/grouploop/MainActivity.java
@@ -3,18 +3,31 @@ package ferocioushammerheads.grouploop;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Firebase session";
+
     private Button ChangeGroupItems;
     private Button ChangeLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+        } else {
+            Log.d(TAG, "onAuthStateChanged:signed_out");
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 

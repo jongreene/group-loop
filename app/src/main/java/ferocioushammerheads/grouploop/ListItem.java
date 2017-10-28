0f8a653ae9fc79e91611fa66bbf71e6f783eb987
Ryan Kass <r954k200@ku.edu>
2017-10-28 17:00:31 -0500

    Began designing list view

--- app/src/main/java/ferocioushammerheads/grouploop/ListItem.java
+++ app/src/main/java/ferocioushammerheads/grouploop/ListItem.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ListItem extends AppCompatActivity {
+    //ArrayList<String> listItems=new ArrayList<String>();
+    //ArrayAdapter<String> adapter;
+    int clickCounter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_item);
     }
+   // public void addItems(View v) {
+        //listItems.add("Clicked : " + clickCounter++);
+       // adapter.notifyDataSetChanged();
+   // }
 }

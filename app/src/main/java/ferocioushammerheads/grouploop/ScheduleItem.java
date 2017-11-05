903ea7acc9414dafa5defecfea660b866c3cceb3
Kate <kate.ramge@gmail.com>
2017-11-05 14:50:49 -0600

    chaos and trying to figure everything out

--- app/src/main/java/ferocioushammerheads/grouploop/ScheduleItem.java
+++ app/src/main/java/ferocioushammerheads/grouploop/ScheduleItem.java
@@ -2,6 +2,8 @@ package ferocioushammerheads.grouploop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.*;
+import android.view.View;
 
 public class ScheduleItem extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class ScheduleItem extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_item);
     }
+
 }
--- app/src/main/java/ferocioushammerheads/grouploop/Schedule_date.java
+++ app/src/main/java/ferocioushammerheads/grouploop/Schedule_date.java
@@ -2,6 +2,8 @@ package ferocioushammerheads.grouploop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.*;
+import android.view.View;
 
 public class Schedule_date extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Schedule_date extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_date);
+    }
+
+    public void choseDate(View v) {
+        //TODO: database stuff
+        //TODO: other stuff
     }
 }

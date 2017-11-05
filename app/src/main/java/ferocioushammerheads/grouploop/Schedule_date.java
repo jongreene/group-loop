fb3466eac8e5315b80ac3133277a1b1bd7d96328
Kate <kate.ramge@gmail.com>
2017-11-05 13:34:35 -0600

    Visual date picker added

---
+++ app/src/main/java/ferocioushammerheads/grouploop/Schedule_date.java
@@ -0,0 +1,13 @@
+package ferocioushammerheads.grouploop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Schedule_date extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_date);
+    }
+}

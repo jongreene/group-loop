4e92895b911ab13b2ef97835b92cd4d57e11e591
Jon Greene <jongreene@ku.edu>
2017-11-05 19:41:29 -0600

    Generated javadoc. Added app icon.

--- app/src/main/java/ferocioushammerheads/grouploop/MainActivity.java
+++ app/src/main/java/ferocioushammerheads/grouploop/MainActivity.java
@@ -1,3 +1,12 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  Ferocious Hammerheads
+ * @version Beta 1.0
+ * @since   2017-11-5
+ */
+
 package ferocioushammerheads.grouploop;
 
 import android.content.Intent;

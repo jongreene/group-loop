af31c5b8c792e6b768efd68663dea9100770223a
Kate <kate.ramge@gmail.com>
2017-11-05 20:26:21 -0600

    schedule is visually done and has actions attached to buttons. Needs to be linked with database.

--- app/src/main/java/ferocioushammerheads/grouploop/ScheduleItem.java
+++ app/src/main/java/ferocioushammerheads/grouploop/ScheduleItem.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.*;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class ScheduleItem extends AppCompatActivity {
 
@@ -13,27 +15,54 @@ public class ScheduleItem extends AppCompatActivity {
         setContentView(R.layout.activity_schedule_item);
     }
 
+    //this is for transitioning from the checkbox times to the discriptions
     public void onClickAdd(View v) {
         /*
         TODO:
         database stuff
         checkbox stuff
          */
+        findViewById(R.id.datePicker).setVisibility(View.GONE);
         findViewById(R.id.floatingActionButton2).setVisibility(View.GONE);
         findViewById(R.id.scrollView2).setVisibility(View.GONE);
         findViewById(R.id.editText2).setVisibility(View.VISIBLE);
         findViewById(R.id.button2).setVisibility(View.VISIBLE);
+        findViewById(R.id.button3).setVisibility(View.GONE);
     }
 
+    //this is for transitioning from the discriptions to the checkbox times
     public void onClickDone(View v) {
         /*
         TODO:
         database stuff
          */
+        findViewById(R.id.datePicker).setVisibility(View.GONE);
         findViewById(R.id.floatingActionButton2).setVisibility(View.VISIBLE);
         findViewById(R.id.scrollView2).setVisibility(View.VISIBLE);
         findViewById(R.id.editText2).setVisibility(View.GONE);
         findViewById(R.id.button2).setVisibility(View.GONE);
+        findViewById(R.id.button3).setVisibility(View.GONE);
+    }
+
+    //this is for transitioning from the datepicker to the checkbox times
+    public void onClickPickDate(View v) {
+        int yy = ((DatePicker)findViewById(R.id.datePicker)).getYear();
+        int mm = ((DatePicker)findViewById(R.id.datePicker)).getMonth();
+        int dd = ((DatePicker)findViewById(R.id.datePicker)).getDayOfMonth();
+
+        /*String test = ""+dd+"/"+mm+"/"+yy+"";
+        ((TextView)findViewById(R.id.textView2)).setText(test);
+        */
+        /*
+        TODO:
+        database stuff
+         */
+        findViewById(R.id.datePicker).setVisibility(View.GONE);
+        findViewById(R.id.floatingActionButton2).setVisibility(View.VISIBLE);
+        findViewById(R.id.scrollView2).setVisibility(View.VISIBLE);
+        findViewById(R.id.editText2).setVisibility(View.GONE);
+        findViewById(R.id.button2).setVisibility(View.GONE);
+        findViewById(R.id.button3).setVisibility(View.GONE);
     }
 
 }

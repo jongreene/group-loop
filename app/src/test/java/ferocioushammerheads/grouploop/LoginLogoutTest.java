216c51fc4f69d472af2724e7d8a5175c5d8ef7f4
Kate <kate.ramge@gmail.com>
2017-11-12 16:04:11 -0600

    Made skeleton unit test files. Still need to make the test code.

---
+++ app/src/test/java/ferocioushammerheads/grouploop/LoginLogoutTest.java
@@ -0,0 +1,30 @@
+package ferocioushammerheads.grouploop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Katherine on 11/12/2017.
+ */
+public class LoginLogoutTest {
+    /*
+    TODO: make the unit tests
+    */
+    @Test
+    public void onStart() throws Exception {
+    }
+
+    @Test
+    public void onClick() throws Exception {
+    }
+
+    @Test
+    public void showProgressDialog() throws Exception {
+    }
+
+    @Test
+    public void hideProgressDialog() throws Exception {
+    }
+
+}

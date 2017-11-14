28e89a928d9845651b7da47c6475f31ce57b80bb
Jon Greene <jongreene@ku.edu>
2017-11-14 13:40:13 -0600

    changing default activity for dev of view list items

--- app/src/main/java/ferocioushammerheads/grouploop/MainActivity.java
+++ app/src/main/java/ferocioushammerheads/grouploop/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
 
 //        Intent intent = new Intent(this, ChipItems.class);
-        Intent intent = new Intent(this, UserAccount.class);
+        Intent intent = new Intent(this, ChipItems.class);
         startActivity(intent);
 
         updateUserEnvironment();

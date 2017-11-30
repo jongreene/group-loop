7ced98b670558d952b20410501550b1125e181fa
Ryan Kass <r954k200@ku.edu>
2017-11-30 13:04:44 -0600

    trying to update?

--- app/src/main/java/ferocioushammerheads/grouploop/CreateGroup.java
+++ app/src/main/java/ferocioushammerheads/grouploop/CreateGroup.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +15,24 @@ import android.view.LayoutInflater;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import static ferocioushammerheads.grouploop.MainActivity.currentGroup;
 
 public class CreateGroup extends Fragment {
 
 
+    private static final String TAG = "CreateGroup";
     private OnFragmentInteractionListener mListener;
     private ButtonClickListener mButtonClickListener;
     private Button mMakeGroup;
     private EditText mGroupUID;
     private View view;
+    private DatabaseReference mDatabaseRef;
 
     public CreateGroup() {
         // Required empty public constructor
@@ -84,16 +93,37 @@ public class CreateGroup extends Fragment {
 
         @Override
         public void onClick(View view) {
-            AccountTools tmpTools = AccountTools.getInstance();
             int clickedId = view.getId();
             if (clickedId == R.id.makeGroup) {
                 //MainActivity.currentGroup = new UserGroup(MainActivity.user.getUid(),"testGroup3");
                 //UserAccount.mDatabase.child("groups").child("testGroup3").setValue(MainActivity.currentGroup);
-
+                String userRefString = "/groups/";
+                mDatabaseRef = FirebaseDatabase.getInstance().getReference(userRefString);
                 MainActivity.currentGroup = new UserGroup(MainActivity.user.getUid(),mGroupUID.getText().toString());
-                UserAccount.mDatabase.child("groups").child(currentGroup.getGroupId()).setValue(MainActivity.currentGroup);
-                MainActivity.userProfile.addNewGroup(currentGroup.getGroupId());
-                tmpTools.updateUser(MainActivity.userProfile);
+                mDatabaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        if (!snapshot.hasChild(mGroupUID.getText().toString())) {
+                            AccountTools tmpTools = AccountTools.getInstance();
+                            UserAccount.mDatabase.child("groups").child(currentGroup.getGroupId()).setValue(MainActivity.currentGroup);
+                            MainActivity.userProfile.addNewGroup(currentGroup.getGroupId());
+                            tmpTools.updateUser(MainActivity.userProfile);
+
+
+                            Log.w(TAG, "Group doesn't exist");
+                        }
+                        else {
+                            Log.w(TAG, "Group exists");
+                        }
+                        mGroupUID.setText("");
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+
+
                 //todo swap fragment changeGroup
             }
         }

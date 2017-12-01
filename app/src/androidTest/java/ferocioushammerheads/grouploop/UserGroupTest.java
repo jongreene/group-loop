bc055880b9936480d6efecc8b3c293c6d921033c
Jonathon Greene <jongreene@ku.edu>
2017-11-30 23:27:37 -0600

    Created empty tests

---
+++ app/src/androidTest/java/ferocioushammerheads/grouploop/UserGroupTest.java
@@ -0,0 +1,63 @@
+package ferocioushammerheads.grouploop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by uberslam on 11/30/17.
+ */
+public class UserGroupTest {
+    @Test
+    public void addMember() throws Exception {
+    }
+
+    @Test
+    public void addChipItem() throws Exception {
+    }
+
+    @Test
+    public void removeMember() throws Exception {
+    }
+
+    @Test
+    public void setUserId() throws Exception {
+    }
+
+    @Test
+    public void setCreator() throws Exception {
+    }
+
+    @Test
+    public void setGroupId() throws Exception {
+    }
+
+    @Test
+    public void setMembers() throws Exception {
+    }
+
+    @Test
+    public void setChipItems() throws Exception {
+    }
+
+    @Test
+    public void getUserId() throws Exception {
+    }
+
+    @Test
+    public void getCreator() throws Exception {
+    }
+
+    @Test
+    public void getGroupId() throws Exception {
+    }
+
+    @Test
+    public void getMembers() throws Exception {
+    }
+
+    @Test
+    public void getChipItems() throws Exception {
+    }
+
+}

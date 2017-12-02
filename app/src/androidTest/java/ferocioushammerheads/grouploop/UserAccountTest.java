63137f9eade961c9a4c0de28f518610ab3d28160
Kate <kate.ramge@gmail.com>
2017-12-02 17:10:02 -0600

    Commented up everything to try and get an idea where to even start.

--- app/src/androidTest/java/ferocioushammerheads/grouploop/UserAccountTest.java
+++ app/src/androidTest/java/ferocioushammerheads/grouploop/UserAccountTest.java
@@ -10,54 +10,137 @@ import static org.junit.Assert.*;
  * Created by uberslam on 12/2/17.
  */
 public class UserAccountTest {
+
+    //consider making a mock UserProfile to test some fragments functionality
+
+
+    /*
+     * would test
+     * first ->
+     * second ->
+     */
     @Before
     public void setUp() throws Exception {
     }
 
+
+    /*
+     * would test
+     * first ->
+     * second ->
+     */
     @After
     public void tearDown() throws Exception {
     }
 
+
+    /*
+     * would test protected void onCreate(Bundle savedInstanceState)
+     * first -> What is a bundle
+     * second -> What type of things would need to include to pass in test bundles
+     * third -> What type of Exceptions could happen from tested bundles
+     */
     @Test
     public void onCreate() throws Exception {
+
     }
 
+
+    /*
+     * would test public boolean onCreateOptionsMenu(Menu menu)
+     * first ->
+     * second ->
+     */
     @Test
     public void onCreateOptionsMenu() throws Exception {
     }
 
+
+    /*
+     * would test public boolean onOptionsItemSelected(MenuItem item)
+     * first ->
+     * second ->
+     */
     @Test
     public void onOptionsItemSelected() throws Exception {
     }
 
+
+    /*
+     * would test public void onBackPressed()
+     * first ->
+     * second ->
+     */
     @Test
     public void onBackPressed() throws Exception {
     }
 
+
+    /*
+     * would test public void fragmentChanger(Class newFragment, int containerName, String fragName)
+     * first ->
+     * second ->
+     */
     @Test
     public void fragmentChanger() throws Exception {
     }
 
+
+    /*
+     * would test public void onFragmentInteraction()
+     * first ->
+     * second ->
+     */
     @Test
     public void onFragmentInteraction() throws Exception {
     }
 
+
+    /*
+     * would test public void onFragmentInteraction(UserProfile profile)
+     * first ->
+     * second ->
+     */
     @Test
     public void onFragmentInteraction1() throws Exception {
     }
 
+
+    /*
+     * would test public void onFragmentInteraction(View view)
+     * first ->
+     * second ->
+     */
     @Test
     public void onFragmentInteraction2() throws Exception {
     }
 
+
+    /*
+     * would test public void loggedInEvent()
+     * first ->
+     * second ->
+     */
     @Test
     public void loggedInEvent() throws Exception {
     }
 
+
+    /*
+     * would test public void loadProfileEvent()
+     * first ->
+     * second ->
+     */
     @Test
     public void loadProfileEvent() throws Exception {
     }
 
+
+    /*
+     * would test public void toastUp(String toastText)
+     * first ->
+     * second ->
+     */
     @Test
     public void toastUp() throws Exception {
     }

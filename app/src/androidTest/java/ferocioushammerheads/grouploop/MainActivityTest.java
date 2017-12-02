179aa518e507965b306fb7b610940b7a43ef1691
Jonathon Greene <jongreene@ku.edu>
2017-12-02 14:15:15 -0600

    Generated testing templates for activities

---
+++ app/src/androidTest/java/ferocioushammerheads/grouploop/MainActivityTest.java
@@ -0,0 +1,45 @@
+package ferocioushammerheads.grouploop;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by uberslam on 12/2/17.
+ */
+public class MainActivityTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void updateUserEnvironment() throws Exception {
+    }
+
+    @Test
+    public void changeActivity() throws Exception {
+    }
+
+    @Test
+    public void loggedInEvent() throws Exception {
+    }
+
+    @Test
+    public void loadProfileEvent() throws Exception {
+    }
+
+    @Test
+    public void toastUp() throws Exception {
+    }
+
+}
---
+++ app/src/androidTest/java/ferocioushammerheads/grouploop/UserAccountTest.java
@@ -0,0 +1,65 @@
+package ferocioushammerheads.grouploop;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by uberslam on 12/2/17.
+ */
+public class UserAccountTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+    }
+
+    @Test
+    public void onBackPressed() throws Exception {
+    }
+
+    @Test
+    public void fragmentChanger() throws Exception {
+    }
+
+    @Test
+    public void onFragmentInteraction() throws Exception {
+    }
+
+    @Test
+    public void onFragmentInteraction1() throws Exception {
+    }
+
+    @Test
+    public void onFragmentInteraction2() throws Exception {
+    }
+
+    @Test
+    public void loggedInEvent() throws Exception {
+    }
+
+    @Test
+    public void loadProfileEvent() throws Exception {
+    }
+
+    @Test
+    public void toastUp() throws Exception {
+    }
+
+}

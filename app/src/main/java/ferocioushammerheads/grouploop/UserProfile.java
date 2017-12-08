eb932eba8f8a6e5040f2f92ac1becc433501a637
Ryan Kass <r954k200@ku.edu>
2017-12-07 21:03:38 -0600

    Added/replaced documentation

--- app/src/main/java/ferocioushammerheads/grouploop/UserProfile.java
+++ app/src/main/java/ferocioushammerheads/grouploop/UserProfile.java
@@ -59,9 +59,11 @@ public class UserProfile {
         String groupListString = "";
         int count = 1;
         for(String group : groupList){
-            groupListString.concat(group);
+//            groupListString.concat(group);
+            groupListString += group;
             if(count < groupList.size()){
-                groupListString.concat(", ");
+//                groupListString.concat(", ");
+                groupListString += ", ";
             }
             count++;
         }
